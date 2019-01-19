95eb7270089cc8cd66b0f4a138f263f45b8b2ce6
Roi Shtivi <roi.shtivi@mail.huji.ac.il>
2019-01-19 15:57:56 +0200

    log the translation of the highlighted text to 'content.js'

--- background.js
+++ background.js
@@ -17,31 +17,13 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
-// // ContextMenu on click listener
-// chrome.contextMenus.onClicked.addListener(function(info) {
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     chrome.tabs.sendMessage(tabs[0].id, {command: "translate", selection: info.selectionText}, function(response) {
-//       console.log(response.trans_selection);
-//     });
-//   });
-// });
+// ContextMenu on click listener
+chrome.contextMenus.onClicked.addListener(function(info) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {command: "translate", selection: info.selectionText}, function(response) {
+      console.log(response.trans_selection);
+    });
+  });
+});
 
-function makeCall() {   
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = processResponse(xhr);
-	xhr.open("GET", 'http://www.morfix.co.il/wow', true);
-	xhr.send(null);
-}
-function processResponse(xhr) {
-	return function(){
-		if (xhr.readyState == XMLHttpRequest.DONE) {
-		    var htmlObject = document.createElement('div');
-		    htmlObject.innerHTML = xhr.responseText;
-		    var translations = htmlObject.getElementsByClassName("translation translation_he heTrans");
-		    for (var i=0; i< translations.length; i++){
-		    	console.log(translations[i].innerHTML);
-		    }
-		}
-	}
-}
-makeCall();
+
--- content.js
+++ content.js
@@ -1,11 +1,38 @@
-// // Listen for messages
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     if (request.command == "translate")
-//       sendResponse({trans_selection: translate(request.selection)});
-//   });
+// Listen for messages
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.command == "translate"){
+    	var trans = makeCall(request.selection);
+    	sendResponse({trans_selection: trans});
+    }
+  });
 
-
-// function translate(selection){
-// 	return "translated:" + selection
-// }
+function makeCall(word) {
+	var xhr = new XMLHttpRequest();
+	var lang = "he";
+	var english = /^[A-Za-z0-9]*$/;
+	if (english.test(word)){
+		lang = "en";
+	}
+	xhr.onreadystatechange = processResponse(xhr, lang);
+	xhr.open("GET", 'http://www.morfix.co.il/' + word, true);
+    xhr.setRequestHeader("Content-Type", "text/plain;charset=ISO-8859-1");
+	xhr.send(null);
+}
+function processResponse(xhr, lang) {
+	return function(){
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+		    var htmlObject = document.createElement('div');
+		    htmlObject.innerHTML = xhr.responseText;
+		    if (lang == "he"){
+		    	var translations = htmlObject.getElementsByClassName("default_trans");
+			}
+			if (lang == "en"){
+				var translations = htmlObject.getElementsByClassName("translation translation_he heTrans");
+			}
+		    for (var i=0; i< translations.length; i++){
+		    	console.log(translations[i].innerHTML);
+		    }
+		}
+	}
+}
